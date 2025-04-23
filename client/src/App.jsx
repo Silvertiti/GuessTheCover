955c2de9c72fb2cf95aeb32e66b6c150d07a356c
Silvertiti <metheotim2@gmail.com>
2025-04-23 21:48:18 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-800 text-white flex items-center justify-center text-4xl font-bold">
+      🎉 Tailwind est prêt !
+    </div>
+  );
+}
+export default App;
